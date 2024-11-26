2e0d2e35f63b345c9dbc097a35693982f59a8c6e
Hong7Cong <nguyentienhong1997@gmail.com>
2024-11-26 19:25:48 +0000

    debug

--- tools/run_tpu.py
+++ tools/run_tpu.py
@@ -460,7 +460,7 @@ def train(cfg):
     # Create the video train and val loaders.
     # train_loader = construct_loader(cfg, "train", device)
     # val_loader = construct_loader(cfg, "val", device)
-    train_loader, val_loader = construct_fake_loader
+    train_loader, val_loader = construct_fake_loader()
 
     # logger.info("Contruct trainloader precise_bn_loader...")
     # precise_bn_loader = (
