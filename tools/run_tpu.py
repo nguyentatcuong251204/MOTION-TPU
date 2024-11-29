10d58dabadcc8f2b456726a8dbdd1eb3d759cff5
Hong7Cong <nguyentienhong1997@gmail.com>
2024-11-29 09:23:54 +0000

    debug

--- tools/run_tpu.py
+++ tools/run_tpu.py
@@ -501,5 +501,5 @@ if __name__ == "__main__":
     xla.launch(
                 _mp_fn,
                 args=(cfg,),
-                debug_single_process=True
+                debug_single_process=0
             )
